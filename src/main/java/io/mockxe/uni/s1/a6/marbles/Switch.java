7fd63e316a483f1eedc55d94fd9c01f935cbfcc7
mockxe <38732499+mockxe@users.noreply.github.com>
2022-11-29 21:16:13 +0100

    added javadoc

--- src/main/java/io/mockxe/uni/s1/a6/marbles/Switch.java
+++ src/main/java/io/mockxe/uni/s1/a6/marbles/Switch.java
@@ -9,19 +9,64 @@ public class Switch {
     private Switch nextSucc;
 
 
-    // classic switch
+    /**
+     * Creates an instance of {@link Switch} class by defining its two successors. <br>
+     * This is the default behaviour of a Switch
+     *
+     * <pre>
+     *     new instance
+     *         /  \
+     *     succ0  succ1
+     * </pre>
+     *
+     * @param succ0 first or "left" successor
+     * @param succ1 second or "right successor
+     */
     public Switch(Switch succ0, Switch succ1) {
         this.succ0 = succ0;
         this.succ1 = succ1;
         this.nextSucc = this.succ0;
     }
 
-    // straight
+    /**
+     * Creates an instance of {@link Switch} with just one successor. <br>
+     * This is also called a "Straight"
+     *
+     * <pre>
+     *     new instance
+     *           |
+     *         succ
+     * </pre>
+     *
+     * @param succ
+     */
     public Switch(Switch succ) {
         this(succ, succ);
     }
 
-    // multi-switch
+    /**
+     * Creates an instance of {@link Switch} with three equally distributed successor. <br>
+     * This is also called a "Multi-Switch"
+     * <br>
+     * <br>
+     * Internally this works by having two switches as successor, giving out three of the outputs and routing back
+     * the fourth output into the input.<br>
+     * This guarantees the output order {@code succ0, succ1, succ2, succ0, succ1, succ2, ...}
+     *
+     * <pre>
+     *        new instance <-\
+     *          /      \     |
+     *         o        o----/
+     *       /   \      |
+     *    succ0 succ2 succ1
+     *
+     * </pre>
+     *
+     *
+     * @param succ0
+     * @param succ1
+     * @param succ2
+     */
     public Switch(Switch succ0, Switch succ1, Switch succ2) {
         this(
                 new Switch(succ0, succ2),
@@ -30,7 +75,13 @@ public class Switch {
         this.succ1.succ1 = this;
     }
 
-
+    /**
+     * Simulates a marble rolling through the switch.<br>
+     * Every time after a marble passed the switch, it switches between the successors, so it will be the other one
+     * the next time.
+     *
+     * @return the successor the next marble will go to
+     */
     public Switch next() {
         Switch ret = nextSucc;
 
@@ -43,6 +94,13 @@ public class Switch {
         return ret;
     }
 
+    /**
+     * Simulates the marble rolling through the switch and all its successors.<br>
+     * Every time after a marble passed the switch, it switches between the successors.
+     * The method will return the last switch the marble has passed, basically where it stopped.
+     *
+     * @return the last switch the marble has passed
+     */
     public Switch findLast() {
         Switch next = this.next();
 
@@ -53,6 +111,11 @@ public class Switch {
         }
     }
 
+    /**
+     * Returns an array of all direct successors of this switch that are not {@code null}
+     *
+     * @return an array of all direct successors of this switch that are not {@code null}
+     */
     public Switch[] directSuccessors() {
         int i = 0;
         Switch[] src = new Switch[2];
@@ -66,10 +129,15 @@ public class Switch {
             i++;
         }
 
-
         return recursiveArrayCopy(src, i);
     }
 
+    /**
+     * Counts all possible switches (including itself) where a marble could go through,
+     * starting from itself. Cycles are only counted once.
+     *
+     * @return the number of switches
+     */
     public int countSwitches() {
         return successors(this).length;
     }
